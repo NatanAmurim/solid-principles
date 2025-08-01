ac9f11ba90ee52d93e3ececdbcd82d140e392b54
natanamurim <natanamurim1998@gmail.com>
2025-08-01 15:15:49 -0300

    feature: adição de novo projeto para estudar clean code e refatoração de solução para melhor separação de estudos.

---
+++ CleanCode/Introduction/CodeThatSpeaksForItself.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode.Introduction
+{
+    /// <summary>
+    /// Ambas as funções fazem a mesma coisa, mas qual é melhor para manutenção?
+    /// </summary>
+    public class Example1
+    {
+        /// <summary>
+        /// O que isso faz?
+        /// </summary>
+        /// <param name="d"></param>
+        public bool FunctionA(int d)
+        {
+            return d < 120;            
+        }
+
+        /// <summary>
+        /// Note que o nome da função é mais claro, você sabe que o que ela faz só de bater o olho.
+        /// Poderia deixar mais enxuto o código? Sim, claro! Mas isso reduziria a clareza.
+        /// É melhor um código mais fácil de ler e dar manutenção. Economizar palavras ou pequenas estruturas pode parecer algo legal,
+        /// mas quando você precisar dar manutenção voltando no futuro para esse código, quanto mais organizado e explicado, melhor!
+        /// </summary>
+        /// <param name="daysSinceLastBackup"></param>
+        /// <returns></returns>
+        public bool IsDayForBackup(int daysSinceLastBackup)
+        {
+            const int MAX_DURATION_IN_DAYS = 120;
+
+            if (daysSinceLastBackup < MAX_DURATION_IN_DAYS)            
+                return true;            
+
+            return false;
+        }
+    }
+}
---
+++ CleanCode/Introduction/SmallWellNamedFunctions.cs
@@ -0,0 +1,93 @@
+﻿namespace CleanCode.Introduction
+{
+    public class ClientServiceBadExample
+    {
+        public void Process(Client client)
+        {
+            // valida o cliente
+            if (client.Id <= 0 || string.IsNullOrEmpty(client.Name))
+            {
+                throw new Exception("Cliente inválido");
+            }
+
+            // calcula desconto
+            var discount = 0.0m;
+            if (client.Loyalty > 5)
+            {
+                discount = 0.1m;
+            }
+
+            // aplica desconto
+            client.Order.Value -= client.Order.Value * discount;
+
+            // salva no banco
+            //repositoy.Save(client.Order);
+        }
+    }
+
+    /// <summary>
+    /// Note que o código cresceu, mas cada função tem um bom nome, é pequena e realiza uma única coisa!
+    /// Qualquer um que for fazer manutenção nesse código o entenderá facilmente.
+    /// E para realizar testes unitários, basta realizar o teste de cada função.    
+    /// </summary>
+    public class ClientServiceGoodExample
+    {
+        public void ProcessClient(Client client)
+        {
+            ValidateClient(client);
+
+            ApplyDiscount(client);
+
+            SaveClient(client);
+        }
+
+        private static void ValidateClient(Client client)
+        {
+            if (client.Id <= 0 || string.IsNullOrEmpty(client.Name))
+            {
+                throw new Exception("Cliente inválido");
+            }
+        }
+
+        private static void SaveClient(Client client)
+        {
+            /* Lógica para salvar em banco*/
+        }
+
+        private static void ApplyDiscount(Client client)
+        {
+            decimal discount = GetDiscountByLoyalty(client);            
+            CalculateDiscount(client, discount);
+        }
+
+        private static void CalculateDiscount(Client client, decimal discount)
+        {
+            client.Order.Value -= client.Order.Value * discount;
+        }
+
+        private static decimal GetDiscountByLoyalty(Client client)
+        {
+            var discount = 0.0m;
+
+            if (client.Loyalty > 5)            
+                discount = 0.1m;            
+
+            return discount;
+        }
+
+
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Loyalty { get; set; }
+        public Order Order { get; set; }
+    }
+
+    public class Order
+    {
+        public decimal Value { get; set; }
+    }
+}
